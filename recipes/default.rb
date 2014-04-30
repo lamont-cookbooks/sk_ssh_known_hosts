a8aae1d99eed5fb7d189874ebca904980b96e993
Lamont Granquist <lamont@scriptkiddie.org>
2014-04-29 17:05:48 -0700

    helps to save result of math

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,7 +34,7 @@ hosts = [ {
 if Chef::Config[:solo]
   Chef::Log.warn 'ssh_known_hosts requires Chef search - Chef Solo does not support search!'
 else
-  hosts + partial_search(
+  hosts += partial_search(
     :node,
     "keys_ssh:* NOT name:#{node.name}",
     keys: {
